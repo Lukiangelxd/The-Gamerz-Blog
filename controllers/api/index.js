0442e4cb6df10e4d1358577eb7fda612e72f2692
zach <zachhoover126@yahoo.com>
2023-09-11 20:07:00 -0400

    added blogposting and deleting routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const blogRoutes = require('./blogsRoutes')
+const userRoutes = require('./usersRoutes');
+const blogRoutes = require('./blogsRoutes');
 
 router.use('/users', userRoutes);
-router.use('/blogs', blogRoutes)
+router.use('/blogs', blogRoutes);
 
 module.exports = router;
