833c60490fc600529139b44faf881155b5b614c4
zach <zachhoover126@yahoo.com>
2023-09-11 20:51:02 -0400

    added home page route

---
+++ controllers/homePage.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const{ BlogPost, User} = require('../models')
+
+const withAuth = require('../helpers/auth');
+
+router.get('/', async(req,res)=> {
+    try{
+
+        const blogData = await BlogPost.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        const blogs = blogData.map((blog) => blog.get({ plain: true}));
+        res.render('homepage',{blogs} )
+    }catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+module.exports = router
