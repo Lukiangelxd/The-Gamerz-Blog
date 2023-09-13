7a1fb745284265989482596ddca385ce4baf62d5
Hannah Mcevoy <lukiangel57@gmail.com>
2023-09-13 15:52:26 -0400

    quill button add

---
+++ public/modal.js
@@ -0,0 +1,18 @@
+// var modalButton = document.getElementById("clickBTN")
+// var textArea = document.querySelector(".text-editor")
+
+// //console.log(modalButton)
+
+// modalButton.addEventListener("click", function(){
+    
+//     if(textArea.classList.value === "text-editor"){
+//      modalButton.textContent = "close";
+//      textArea.classList.toggle("text-editor")
+//     }else{
+//         modalButton.textContent = "Start a Post";
+//         textArea.classList.toggle("text-editor") 
+//     }
+   
+//     // textArea.style.display = "block";
+ 
+// })
