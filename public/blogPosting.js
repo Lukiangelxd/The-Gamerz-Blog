ede9ec6cff11835986cbb84a5c6480a4a3a3fb21
zach <zachhoover126@yahoo.com>
2023-09-18 22:21:38 -0400

    added posting and formatting to every blog page

--- public/blogPosting.js
+++ public/blogPosting.js
@@ -4,35 +4,21 @@ const newBlogHandler = async (event) => {
     //Note: All queryselector's are generic names subject to change based on actual html design.
     //Collects values from the blog post form.
     const title = document.querySelector('#blog-title').value.trim();
+    const subHeading = document.querySelector('#blog-subheading').value.trim()
     const post = document.querySelector('#blog-post').value.trim();
     const tags = document.querySelector('#blog-tags').value.trim();
-    const platform = document.querySelector('#blog-platform').value.trim();
     //This is for setting up the platform_id value.
-    var platform_id;
-    switch (platform) {
-        case 'Nintendo': 
-            platform_id = 1;
-            break;
-        case 'PCGaming':
-            platform_id = 2;
-            break;
-        case 'PlayStat':
-            platform_id = 3;
-            break;
-        case 'Xbox':
-            platform_id = 4;
-            break;
-    }
+    const platform_id = event.target.getAttribute('data-platform')
     if(title && post && tags && platform_id) {
         const response = await fetch('/api/blogs', {
             method: 'POST',
-            body: JSON.stringify({title, post, tags, platform_id}),
+            body: JSON.stringify({title, subHeading, post, tags, platform_id}),
             headers: {
                 'Content-Type': 'application/json',
             },
         });
         if(response.ok) {
-            document.location.replace('/api/user');
+            document.location.reload();
         }
         else {
             alert(response.statusText);
